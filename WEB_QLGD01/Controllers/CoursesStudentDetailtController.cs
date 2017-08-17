7ecec48b134ab99fdc4e4a14418fef8d5af82cb1
tiephoangdev <tiephoang.dev@gmail.com>
2017-08-16 18:21:45 -0700

    oke

--- WEB_QLGD01/Controllers/CoursesStudentDetailtController.cs
+++ WEB_QLGD01/Controllers/CoursesStudentDetailtController.cs
@@ -10,13 +10,59 @@ namespace WEB_QLGD01.Controllers
     public class CoursesStudentDetailtController : Controller
     {
         // GET: CoursesStudentDetailt
-        public ActionResult Index(Guid ID)
+        public ActionResult Index(Guid? ID = null)
         {
-            var ob = new CoursesJournalBCL().GetByCJId(ID);
-            ob.CoursesJoin = new CoursesBCL().GetByCoId((Guid)ob.CoId);
-            ViewBag.CJ = ob;
-            var list = new CoursesStudentDetailtBCL().GetJoin().Where(q => q.CJId.Equals(ID)).ToList();
-            return View(list);
+
+            var allCJ = new CoursesStudentDetailtBCL().GetJoin();
+            var dropCJ = new List<SelectListItem>();
+            Guid idcj = Guid.Empty;
+            if (ID.HasValue)
+            {
+                idcj = (Guid)ID;
+                var ob = new CoursesJournalBCL().GetByCJId(idcj);
+                ob.CoursesJoin = new CoursesBCL().GetByCoId((Guid)ob.CoId);
+                ViewBag.CJ = ob;
+            }
+
+            //Tạo drop nhật kí
+            dropCJ.Add(new SelectListItem()
+            {
+                Text = "All",
+                Value = "",
+                Selected = !ID.HasValue
+            });
+
+            foreach (var item in allCJ)
+            {
+                item.CoursesJournalJoin.CoursesJoin = new CoursesBCL().GetByCoId((Guid)item.CoursesJournalJoin.CoId);
+            }
+
+            foreach (var item in allCJ)
+            {
+                dropCJ.Add(new SelectListItem()
+                {
+                    Text = "Nhật kí ngày " + item.CoursesJournalJoin.DayOf.Value.ToString("d") + " - Lớp:" + item.CoursesJournalJoin.CoursesJoin.CourseId + " - Nội dung: " + item.CoursesJournalJoin.Contents,
+                    Value = item.CJId.ToString(),
+                    Selected = item.CJId == idcj
+                });
+            }
+            ViewBag.dropCJ = dropCJ;
+
+            //lấy nhật kí nếu có ID
+            if (ID.HasValue)
+            {
+                allCJ = allCJ.Where(q => q.CJId.Equals(idcj)).ToList();
+            }
+
+
+            return View(allCJ);
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection f)
+        {
+            if (f["sCJ"] == null || string.IsNullOrWhiteSpace(f["sCJ"].ToString()) || f["sCJ"].ToString().Equals(Guid.Empty.ToString())) return RedirectToAction("Index", new { ID = (Guid?)null });
+            return RedirectToAction("Index", new { ID = Guid.Parse(f["sCJ"].ToString()) });
         }
 
         public ActionResult Create(Guid ID)
@@ -37,6 +83,7 @@ namespace WEB_QLGD01.Controllers
 
             ViewBag.CJ = ob;
             ViewBag.dropStuden = dropStuden;
+            if (dropStuden.Count <= 0) ModelState.AddModelError("StudetId", "Không có học viên nào trong lớp học để thêm!");
 
             return View(new CoursesStudentDetailtObject() { CJId = ob.CJId });
 
